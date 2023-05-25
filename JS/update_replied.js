02237a73a37b7d954ad6eba6125f3a773e6d245b
Alcides Ojeda <alcides.ojeda@mail.com>
2023-05-25 13:53:53 +0200

    update timing replied airtable js

--- JS/update_replied.js
+++ JS/update_replied.js
@@ -1,27 +1,29 @@
 let link_table = base.getTable("tbl8NAwDIqxcLasZL");
 let email_queue_table = base.getTable("tblJaDA1FVHJIbx3b");
 let inputConfig = input.config();
+let backlink_id = inputConfig.backlink_id[0] ?? '';
 let email_id = inputConfig.email_id[0] ?? ''
 let date = inputConfig.date;
 let gmail_id = inputConfig.gmail_id;
 let date_replied = inputConfig.date_replied;
 if(email_id){
     let records_to_update = []
-    let link = await link_table.selectRecordAsync(email_id)
+    let link = await link_table.selectRecordAsync(backlink_id)
     let emails = link ? link.getCellValue("fldr3n17WgLEs6yp5") : null;
-    let total_emails = emails?.length ?? null
+    let total_emails = emails?.length ?? null;
+    console.log(`total emails `,total_emails)
     if (total_emails){
         emails.forEach(email =>{
-            if(email !== email_id){
+            if(email.id !== email_id){
                 records_to_update.push({
-                    id: email,
+                    id: email.id,
                     fields:{
                         "fldeoNVjJpbMNBv8d": {name: "Sequence Replied"}
                     }
                 })
             }else{
                 records_to_update.push({
-                    id: email,
+                    id: email.id,
                     fields:{
                         "fldeoNVjJpbMNBv8d": {name: "Sequence Replied"},
                         "fldXg1AA6R8w8NoYg": date_replied
@@ -32,9 +34,11 @@ if(email_id){
         })
     }
     if(link?.id){
+        console.log(`uptading backlin ${link.name}`)
         await link_table.updateRecordAsync(link.id,{"fldbtSyNjnyorTPvb": date})
     }
     if(records_to_update?.length){
+        console.log(`updating emails queue`, records_to_update)
         await email_queue_table.updateRecordsAsync(records_to_update)
     }
 
