c1bbc49cbbf0780711d3bda3cf5a5915d6233b34
Alcides Ojeda <alcides.ojeda@mail.com>
2023-05-25 12:52:28 +0200

    update send email console prints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 from helpers.gmail_handler import send_email
 from helpers.airtable_handler import AirtableHandler
 from dotenv import load_dotenv
@@ -9,10 +9,10 @@ load_dotenv()
 
 def main():
     get_today_emails_to_send()
-    pass
 
 
 def get_today_emails_to_send():
+    date_now = datetime.now().strftime("%d-%m-%y %H:%M:%S")
     email_table = os.getenv("EMAIL_QUEUE_TABLE")
     airtable_handler = AirtableHandler(email_table)
     emails = airtable_handler.get_records(filter_by_formula=" AND(OR( IS_BEFORE({Send Date}, TODAY()), "
@@ -20,6 +20,8 @@ def get_today_emails_to_send():
     if len(emails) > 0:
         emails_generated = parse_emails(emails)
         update_airtable(emails_generated)
+    else:
+        print(f"[!] {date_now} No emails to send today")
 
 
 def parse_emails(emails):
@@ -35,9 +37,6 @@ def parse_emails(emails):
             body = fields.get("body")
             subject = fields.get("subject")
             cc = fields.get("cc")
-            if sequence > 1:
-                # TODO: check replied
-                pass
             email_sent = send_email(to, subject, body, cc)
             emails_generated.append({"at_id": at_email_id, "google_email_id": email_sent["id"],
                                      "sequence": sequence, "at_link_id": at_record_id,
@@ -60,6 +59,7 @@ def update_airtable(emails_generated):
             airtable_records_to_update.append({"id": at_email_id, "fields": fields})
     if len(airtable_records_to_update) > 0:
         airtable_handler.update_records(airtable_records_to_update)
+        print(f"[!] {datetime.now().strftime('%d-%m-%y %H:%M:%S')} Emails sent: {len(airtable_records_to_update)}")
 
 
 if __name__ == "__main__":
