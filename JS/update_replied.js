5ed46a4a075a2e0afaaef27c412eb092a174e3f2
Alcides Ojeda <alcides.ojeda@mail.com>
2023-05-25 13:33:03 +0200

    update timing replied airtable js

--- JS/update_replied.js
+++ JS/update_replied.js
@@ -3,7 +3,8 @@ let email_queue_table = base.getTable("tblJaDA1FVHJIbx3b");
 let inputConfig = input.config();
 let email_id = inputConfig.email_id[0] ?? ''
 let date = inputConfig.date;
-let gmail_id = inputConfig.gmail_id
+let gmail_id = inputConfig.gmail_id;
+let date_replied = inputConfig.date_replied;
 if(email_id){
     let records_to_update = []
     let link = await link_table.selectRecordAsync(email_id)
@@ -11,12 +12,23 @@ if(email_id){
     let total_emails = emails?.length ?? null
     if (total_emails){
         emails.forEach(email =>{
-            records_to_update.push({
-                id: email,
-                fields:{
-                    "fldeoNVjJpbMNBv8d": {name: "Sequence Replied"}
-                }
-            })
+            if(email !== email_id){
+                records_to_update.push({
+                    id: email,
+                    fields:{
+                        "fldeoNVjJpbMNBv8d": {name: "Sequence Replied"}
+                    }
+                })
+            }else{
+                records_to_update.push({
+                    id: email,
+                    fields:{
+                        "fldeoNVjJpbMNBv8d": {name: "Sequence Replied"},
+                        "fldXg1AA6R8w8NoYg": date_replied
+                    }
+                })
+            }
+
         })
     }
     if(link?.id){
